3e49f26e3e7e56372b60a31c7291b7dfb3f86431
Matthew Inamdar <matt@goldspecdigital.com>
2020-03-15 14:11:15 +0000

    Reindex command skipped if not using elastic Scout driver

--- app/Console/Commands/Tlr/ReindexElasticsearchCommand.php
+++ app/Console/Commands/Tlr/ReindexElasticsearchCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\Tlr;
 use App\Models\IndexConfigurators\ServicesIndexConfigurator;
 use App\Models\Service;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 use Throwable;
 
 class ReindexElasticsearchCommand extends Command
@@ -28,6 +29,11 @@ class ReindexElasticsearchCommand extends Command
      */
     public function handle()
     {
+        if (Config::get('scout.driver') !== 'elastic') {
+            $this->warn('Did not reindex due to not using the [elastic] Scout driver.');
+            return;
+        }
+
         try {
             $this->line('Dropping index...');
             $this->call('elastic:drop-index', ['index-configurator' => ServicesIndexConfigurator::class]);
