3787cef8896e4fc3ab2098c2f1bfcf51528d0d2b
Matthew Inamdar <matt@goldspecdigital.com>
2020-03-30 17:30:50 +0100

    Revert "Update UserTest.php"
    This reverts commit f723d0d184d60b2efdcbc858b6578562eee32a66.

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -3,14 +3,13 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Role;
-use App\Models\Service;
 use App\Models\User;
 use App\Models\UserRole;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    public function test_other_user_roles_are_not_removed_when_making_another_user_the_same_role_as_global_admin()
+    public function test_other_user_roles_are_not_removed_when_making_another_user_the_same_role()
     {
         /** @var \App\Models\User $userA */
         $userA = factory(User::class)->create();
@@ -28,32 +27,6 @@ class UserTest extends TestCase
         $this->assertDatabaseHas(table(UserRole::class), [
             'user_id' => $userB->id,
             'role_id' => Role::globalAdmin()->id,
-        ]);
-    }
-
-    public function test_other_user_roles_are_not_removed_when_making_another_user_the_same_role_as_service_worker()
-    {
-        /** @var \App\Models\Service $service */
-        $service = factory(Service::class)->create();
-
-        /** @var \App\Models\User $userA */
-        $userA = factory(User::class)->create();
-
-        /** @var \App\Models\User $userB */
-        $userB = factory(User::class)->create();
-
-        $userA->makeServiceWorker($service);
-        $userB->makeServiceWorker($service);
-
-        $this->assertDatabaseHas(table(UserRole::class), [
-            'user_id' => $userA->id,
-            'role_id' => Role::serviceWorker()->id,
-            'service_id' => $service->id,
-        ]);
-        $this->assertDatabaseHas(table(UserRole::class), [
-            'user_id' => $userB->id,
-            'role_id' => Role::serviceWorker()->id,
-            'service_id' => $service->id,
         ]);
     }
 }
