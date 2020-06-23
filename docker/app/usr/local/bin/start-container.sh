6e7776c7954cafbb08f8f602200a6ff49bcecc9e
Matthew Inamdar <matt@goldspecdigital.com>
2020-06-23 23:11:16 +0100

    Removed Blackfire

--- docker/app/usr/local/bin/start-container.sh
+++ docker/app/usr/local/bin/start-container.sh
@@ -7,8 +7,5 @@ php /var/www/html/artisan migrate --force
 php /var/www/html/artisan config:cache
 php /var/www/html/artisan route:cache
 
-# Start Blackfire.
-/etc/init.d/blackfire-agent start
-
 # Start supervisor.
 /usr/bin/supervisord
