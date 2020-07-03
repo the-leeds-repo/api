cfd0795fbb41f3f1da2a6234cebc2572a5c169d3
tpage1975 <52198244+tpage1975@users.noreply.github.com>
2020-07-03 12:11:25 +0100

    Update NotifyRefereeEmail.php
    Updated with LOOP details.

--- app/Emails/ReferralCompleted/NotifyRefereeEmail.php
+++ app/Emails/ReferralCompleted/NotifyRefereeEmail.php
@@ -26,14 +26,11 @@ The referral you made to ((SERVICE_NAME)) has been marked as complete. Referral 
 
 Your client should have been contacted by now, but if they haven’t then please contact them on ((SERVICE_PHONE)) or by email at ((SERVICE_EMAIL)).
 
-If you would like to leave any feedback on the referral or get in touch with us, you can contact us at info@connectedtogether.org.uk.
-
-Alternatively, you can complete our feedback form:
-https://docs.google.com/forms/d/e/1FAIpQLSe38Oe0vsqLRQbcBjYrGzMooBJKkYqFWAlHy4dcgwJnMFg9dQ/viewform?usp=pp_url&entry.400427747=((REFERRAL_ID)).
+If you would like to leave any feedback on the referral or get in touch with us, you can contact us at info@looprepository.org.
 
 Many thanks,
 
-The Connected Together team
+The LOOP team
 EOT;
     }
 
