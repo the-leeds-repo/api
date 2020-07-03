25b318bea9e61ae872cfeeb1195d2da47581c42c
tpage1975 <52198244+tpage1975@users.noreply.github.com>
2020-07-03 12:12:06 +0100

    Update NotifyClientEmail.php
    Updated with LOOP details.

--- app/Emails/ReferralCreated/NotifyClientEmail.php
+++ app/Emails/ReferralCreated/NotifyClientEmail.php
@@ -28,14 +28,11 @@ They should be in touch with you via ((REFERRAL_CONTACT_METHOD)) within 10 worki
 
 Your referral ID is ((REFERRAL_ID)).
 
-If you have any feedback regarding this connection, or have not heard back within 10 working days, please contact the admin team via info@connectedtogether.org.uk.
-
-Alternatively, you can complete the referral feedback form:
-https://docs.google.com/forms/d/e/1FAIpQLSe38Oe0vsqLRQbcBjYrGzMooBJKkYqFWAlHy4dcgwJnMFg9dQ/viewform?usp=pp_url&entry.400427747=((REFERRAL_ID)).
+If you have any feedback regarding this connection, or have not heard back within 10 working days, please contact the admin team via info@looprepository.org.
 
 Many thanks,
 
-The Connected Together team
+The LOOP team
 EOT;
     }
 
