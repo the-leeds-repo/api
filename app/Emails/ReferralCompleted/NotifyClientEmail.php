73b9de820c96dfe818be2dba6863b29a3a1962fa
tpage1975 <52198244+tpage1975@users.noreply.github.com>
2020-07-03 12:10:43 +0100

    Update NotifyClientEmail.php
    Updated with LOOP details.

--- app/Emails/ReferralCompleted/NotifyClientEmail.php
+++ app/Emails/ReferralCompleted/NotifyClientEmail.php
@@ -28,14 +28,11 @@ Your connection to ((SERVICE_NAME)) has been marked as completed by the service.
 
 This means that they have been in touch with you about accessing their service.
 
-If you have any feedback regarding this connection or believe the service did not try to contact you, please contact the admin team via info@connectedtogether.org.uk.
-
-Alternatively, you can complete the referral feedback form:
-https://docs.google.com/forms/d/e/1FAIpQLSe38Oe0vsqLRQbcBjYrGzMooBJKkYqFWAlHy4dcgwJnMFg9dQ/viewform?usp=pp_url&entry.400427747=((REFERRAL_ID))
+If you have any feedback regarding this connection or believe the service did not try to contact you, please contact the admin team via info@looprepository.org.
 
 Many thanks,
 
-The Connected Together team
+The LOOP team
 EOT;
     }
 
