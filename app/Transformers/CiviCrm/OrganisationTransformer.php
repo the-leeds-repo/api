c04365c6cd0e8f1f53290307ce46803015d32cc6
Matthew Inamdar <matt@goldspecdigital.com>
2020-09-12 16:30:39 +0100

    Added logic for transformer

--- app/Transformers/CiviCrm/OrganisationTransformer.php
+++ app/Transformers/CiviCrm/OrganisationTransformer.php
@@ -12,6 +12,34 @@ class OrganisationTransformer
      */
     public function transform(Organisation $organisation): array
     {
-        return [];
+        return [
+            'name' => $organisation->name,
+            'description' => $organisation->description,
+            'email' => $organisation->email,
+            'url' => $organisation->url,
+            'phone' => $organisation->phone,
+            'address' => $this->transformAddress($organisation),
+        ];
+    }
+
+    /**
+     * @param \App\Models\Organisation $organisation
+     * @return string|null
+     */
+    protected function transformAddress(Organisation $organisation): ?string
+    {
+        $addressParts = [
+            $organisation->address_line_1,
+            $organisation->address_line_2,
+            $organisation->address_line_3,
+            $organisation->city,
+            $organisation->county,
+            $organisation->postcode,
+            $organisation->country,
+        ];
+
+        $addressParts = array_filter($addressParts);
+
+        return implode(', ', $addressParts) ?: null;
     }
 }
--- tests/Unit/Transformers/CiviCrm/OrganisationTransformerTest.php
+++ tests/Unit/Transformers/CiviCrm/OrganisationTransformerTest.php
@@ -10,14 +10,50 @@ class OrganisationTransformerTest extends TestCase
 {
     public function test_transform_works()
     {
-        $organisation = factory(Organisation::class)->create();
+        $organisation = new Organisation([
+            'name' => 'Acme Org',
+            'description' => 'Lorem ipsum',
+            'email' => 'acme.org@example.com',
+            'url' => 'acme.example.com',
+            'phone' => '01130000000',
+            'address_line_1' => '1 Fake Street',
+            'city' => 'Leeds',
+            'postcode' => 'LS1 2AB',
+        ]);
 
         $transformer = new OrganisationTransformer();
         $results = $transformer->transform($organisation);
 
-        $this->assertEqualsCanonicalizing(
-            [],
-            $results
-        );
+        $this->assertEqualsCanonicalizing([
+            'name' => 'Acme Org',
+            'description' => 'Lorem ipsum',
+            'email' => 'acme.org@example.com',
+            'url' => 'acme.example.com',
+            'phone' => '01130000000',
+            'address' => '1 Fake Street, Leeds, LS1 2AB',
+        ], $results);
+    }
+
+    public function test_transform_works_with_no_address()
+    {
+        $organisation = new Organisation([
+            'name' => 'Acme Org',
+            'description' => 'Lorem ipsum',
+            'email' => 'acme.org@example.com',
+            'url' => 'acme.example.com',
+            'phone' => '01130000000',
+        ]);
+
+        $transformer = new OrganisationTransformer();
+        $results = $transformer->transform($organisation);
+
+        $this->assertEqualsCanonicalizing([
+            'name' => 'Acme Org',
+            'description' => 'Lorem ipsum',
+            'email' => 'acme.org@example.com',
+            'url' => 'acme.example.com',
+            'phone' => '01130000000',
+            'address' => null,
+        ], $results);
     }
 }
