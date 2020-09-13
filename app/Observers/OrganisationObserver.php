3486f0183f76aca2f5e7a3ed003b46cbf47bf9e1
Matthew Inamdar <matt@goldspecdigital.com>
2020-09-13 09:37:37 +0100

    Removed unused import

--- app/Observers/OrganisationObserver.php
+++ app/Observers/OrganisationObserver.php
@@ -4,7 +4,6 @@ namespace App\Observers;
 
 use App\CiviCrm\CiviException;
 use App\CiviCrm\ClientInterface;
-use App\Models\FailedCiviSync;
 use App\Models\Organisation;
 
 class OrganisationObserver
